2b0ac646d068bf0e3474b45e9512f03958349c8d
Ian Hancock <northroadian@gmail.com>
2023-03-22 07:05:35 -0500

    routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-    // this is the root level of the url /users
-    // Get /users
-    @Get()
-    getUsers(){
-        return[]
-    }
-    // Get /users/:id --> {...}
-    @Get(':id')
-    getUserById(){
+  // this is the root level of the url /users
+  // Get /users
+  @Get()
+  getUsers() {
+    return ['Hello User'];
+  }
+  // Get /users/:id --> {...}
+  @Get(':id')
+  getUserById() {
+    return {};
+  }
+  // Post /users
+  @Post()
+  createUser(){
+    return{}
+  }
 
-    }
+  @Put(':id')
+  updateUser(){
+    return{}
+  }
+// Delete /users/:id
+  @Delete(':id')
+  deleteUser(){
+    return{}
 
-
+  }
 }
