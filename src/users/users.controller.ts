07c14058659e81b02d03029e12854f73b7347d74
Ian Hancock <northroadian@gmail.com>
2023-03-22 07:00:28 -0500

    routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    // this is the root level of the url /users
+    // Get /users
+    @Get()
+    getUsers(){
+        return[]
+    }
+    // Get /users/:id --> {...}
+    @Get(':id')
+    getUserById(){
+
+    }
+
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,3 +2,5 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {}
+// Get /users  which is an array 
+// get 
