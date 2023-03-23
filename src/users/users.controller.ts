a010cc65ec7b9cba2e1602fc97ccce50bee3fe40
Ian Hancock <northroadian@gmail.com>
2023-03-23 12:08:07 -0500

    dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,32 +1,50 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+    Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   // this is the root level of the url /users
   // Get /users
+
   @Get()
-  getUsers() {
-    return ['Hello User'];
+  getUsers(@Query('type') type: string) {
+    return [{ type }];
   }
+
+  // Get users with a filtering method
+  // users/?type=
+
   // Get /users/:id --> {...}
   @Get(':id')
-  getUserById() {
-    return {};
+  getUserById(@Param('id') id: string) {
+    return {
+      id,
+    };
   }
-  // Post /users
+
+
   @Post()
-  createUser(){
-    return{}
+  createUser(@Body() createUserDto:CreateUserDto){
+    return {
+      name: createUserDto.name
+    }
   }
-
-  @Put(':id')
-  updateUser(){
-    return{}
-  }
-// Delete /users/:id
-  @Delete(':id')
-  deleteUser(){
-    return{}
-
-  }
+  // @Put(':id')
+  // updateUser() {
+  //   return {};
+  // }
+  // // Delete /users/:id
+  // @Delete(':id')
+  // deleteUser() {
+  //   return {};
+  // }
 }
